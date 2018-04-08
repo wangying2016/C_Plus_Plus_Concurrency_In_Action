ef0382728263727993f1a64b876dae5c8b2405db
Wang Ying <wy867504146@outlook.com>
2018-04-08 21:14:58 +0800

    Chapter3 codes

--- Chapter3/practice.cpp
+++ Chapter3/practice.cpp
@@ -324,6 +324,7 @@ private:
         std::lock_guard<std::mutex> lock_a(m);
         return some_detail;
     }
+    
 public:
     Y(int sd) : some_detail(sd) {}
 
@@ -333,5 +334,113 @@ public:
         int const lhs_value = lhs.get_detail();
         int const rhs_value = rhs.get_detail();
         return lhs_value == rhs_value;
+    }
+};
+
+std::shared_ptr<some_resource> resource_ptr;
+void foo()
+{
+    if (!resource_ptr) {
+        resource_ptr.reset(new some_resource);
+    }
+    resource_ptr->do_something();
+}
+
+// Listing 3.11 Thread-safe lazy initialization using a mutex
+std::shared_ptr<some_resource> resource_ptr;
+std::mutex resource_mutex;
+void foo()
+{
+    std::unique_lock<std::mutex> lk(resource_mutex);
+    if (!resource_ptr) {
+        resource_ptr.reset(new some_resource);
+    }
+    lk.unlock();
+    resource_ptr->do_something();
+}
+
+void undefined_behaviour_with_double_checked_locking()
+{
+    if (!resource_ptr) {
+        st::lock_guard<std::mutex> lk(resource_mutex);
+        if (!resource_ptr) {
+            resource_ptr.reset(new some_resource);
+        }
+    }
+    resource_ptr->do_something();
+}
+
+std::shared_ptr<some_resource> resource_ptr;
+std::once_flag resource_flag;
+
+void init_resource()
+{
+    resource_ptr.reset(new some_resource);
+}
+
+void foo()
+{
+    std::call_once(resource_flag, init_resource);
+    resource_ptr->do_something();
+}
+
+// Listing 3.12 Thread-safe lazy initialization of a class member using std::call_once
+class x
+{
+private:
+    connection_info connectioin_details;
+    connection_handle connection;
+    std::once_flag connection_init_flag;
+
+    void open_connection()
+    {
+        connection = connection_manager.open(connection_details);
+    }
+public:
+    X(connection_info const& connection_details_):
+        connection_details(connectioni_detailes_) {}
+    void send_data(data_packet const& data)
+    {
+        std::call_once(connection_init_flag, &X::open_connection, this);
+        connection.send_data(data);
+    }
+    data_packet receive_data()
+    {
+        std::call_once(connection_ini_flag, &X::open_connection, this);
+        return connectioin.receive_data();
+    }
+};
+
+class my_class;
+my_class& get_my_class_instance()
+{
+    static my_class instance;   // 线程安全的初始化过程
+    return instance;
+}
+
+// Listing 3.13 Protecting a data structure with a boost::shared_mutex
+#include <map>
+#include <string>
+#include <mutex>
+#include <boost/thread/shared_mutex.hpp>
+
+class dns_entry;
+
+class dns_cache
+{
+    std::map<std::string, dns_entry> entries;
+    mutable boost::shared_mutex entry_mutex;
+public:
+    dns_entry find_entry(std::string const& domain) const 
+    {
+        boost::shared_lock<boost::shared_mutex> lk(entry_mutex);
+        std::map<std::string, dns_entry>::const_iterator const it = entries.find(domain);
+        return (it == entries.end()) ? dns_entry() : it->second;
+    }
+    void update_or_add_entry(std::string const& domain,
+                             dns_entry const& dns_details)
+    {
+        std::lock_guard<boost::shared_mutex> lk(entry_mutex);
+        entries[domain] = dns_details;
     }
 };
